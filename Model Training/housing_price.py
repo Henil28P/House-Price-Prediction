40aa10c535452f9b76058539f86ed012602727d5
Henil <henilp467@gmail.com>
2024-12-17 15:03:38 +1100

    [housing_price.py]: imported necessary modules and packages for project and commented notes on Python virtual env

--- Model Training/housing_price.py
+++ Model Training/housing_price.py
@@ -1,1 +1,17 @@
 # This script is for training the House Price Prediction model
+
+# To install all the ML libraries, packages and its dependencies:
+# Run the command on the terminal (if Python script): python3 -m pip install -U jupyter matplotlib numpy pandas scipy scikit-learn
+# Make sure to be in the Python virtual environment and then run that command
+# Steps for creating a Python virtual environment and activating it:
+# 1. (On Mac): $ python3 -m venv .venv (to create the virtual environment)
+# 2. (On Mac): $ source .venv/bin/activate (to go in the virtual environment and start installing packages and modules)
+
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+# import sklearn as skl
+# import seaborn as sbn
+import os
+import tarfile
+from six.moves import urllib
